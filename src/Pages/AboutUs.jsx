d64b3c314c7170b52e5ad53a17ae9f816df7a4d3
Xiomara S <xiomisalamanca21@hotmail.com>
2023-12-13 17:15:25 -0500

    se creó la rama proyect-6

--- src/Pages/AboutUs.jsx
+++ src/Pages/AboutUs.jsx
@@ -1,41 +1,57 @@
+import { Heading, Box, Text } from "@chakra-ui/react";
+
 function AboutUs() {
 
+const paragraphStyle = {
+        textAlign: 'justify'
+    };
     return (
-        <div>
-            <>
-            <br />
-            <h2>Descripción de la app</h2>
-            <br />
-             <p>
-                  La utilidad de mi aplicación de gestión de tareas radica en su capacidad para facilitar la   organización 
-                  de tu vida diaria y garantizar que cumplas con todas tus responsabilidades. 
-                  Esta herramienta te permite estructurar listas de tareas, insertar nuevas actividades, 
-                  modificar las existentes, suprimir aquellas que ya no son pertinentes y marcar las que has finalizado. 
-                  Asimismo, la aplicación te proporciona un recuento actualizado de las tareas pendientes y te alerta si 
-                  intentas ingresar una tarea que es excesivamente breve. Añadir información adicional a cada tarea es 
-                  opcional, aunque la posibilidad está disponible para proporcionar detalles extra cuando lo consideres 
-                  necesario.
-</p>
-             <br />
-             <h2>Cómo usar la aplicación</h2>
-             <br />
-             <p>
-                Para emplear la aplicación, inicia creando una lista de tareas y añade las actividades que desees gestionar. 
+            < Box p="4">
+            <Heading as="h2" size="lg" mb="4">
+                     Descripción de la app!
+           </Heading>
+             <Text style={paragraphStyle} mb="4">
+                  La utilidad de mi aplicación de gestión de tareas radica en su capacidad para facilitar la   
+                  organización de tu vida diaria y garantizar que cumplas con todas tus responsabilidades. 
+                  Esta herramienta te permite estructurar listas de tareas, insertar nuevas actividades, modificar las 
+                  existentes, suprimir aquellas que ya no son pertinentes y marcar las que has finalizado. 
+                  Asimismo, la aplicación te proporciona un recuento actualizado de las tareas pendientes y te alerta 
+                  si intentas ingresar una tarea que es excesivamente breve. Añadir información adicional a cada tarea 
+                  es opcional, aunque la posibilidad está disponible para proporcionar detalles extra cuando lo consideres
+                 necesario.
+</Text>
+             <Heading as="h2" size="lg" mb="4">
+                  Cómo usar la aplicación
+            </Heading>
+             <Text style={paragraphStyle} mb="4">
+                Para emplear la aplicación, inicia creando una lista de tareas y añade las actividades que desees gestionar.
                 En cualquier momento, tienes la flexibilidad de editar, eliminar o marcar las tareas como completadas. 
                 La aplicación te proporcionará constantemente el recuento de tareas pendientes, brindándote una visión clara 
                 de la carga de trabajo que tienes por delante.
-</p>
-             <br />
-             <h2>Tecnologías aplicadas</h2>
-             <br />
-               <p>La aplicación de lista de tareas se implementaron con las siguientes tecnologías:</p>
-             <h3>HTML5</h3>
-             <h3>JavaScript</h3>
-             <h3>CSS</h3>
-             <h3>React</h3>
-             </>
-        </div>
+</Text>
+             
+             <Heading as="h2" size="lg" mb="4">
+                   Tecnologías aplicadas
+             </Heading>
+             
+             <Text style={paragraphStyle} mb="4">
+                   La aplicación de lista de tareas se implementó con las siguientes tecnologías:
+               </Text>
+             <Text style={paragraphStyle} mb="4">
+                  HTML5
+             </Text>
+             <Text style={paragraphStyle} mb="4">
+                 JavaScript
+            </Text>
+             <Text style={paragraphStyle} mb="4">
+                CSS
+            </Text>
+             <Text style={paragraphStyle} mb="4">
+                React
+             </Text>
+             
+        </Box>
     )
   }
-  
-  export default AboutUs;
+export default AboutUs;
+
